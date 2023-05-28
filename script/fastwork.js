990e9b7eb192b084835f89e54df48b1b14c2c2fa
Asadbek <asadbekabdulamiyev2901@gmail.com>
2023-05-28 06:53:13 +0500

    fastwork qismi qo'shildi

---
+++ script/fastwork.js
@@ -0,0 +1,16 @@
+let circularProgress = document.querySelector(".circular-progress"),
+  progressValue = document.querySelector(".progress-value");
+
+  let progressStartValue = 0,
+      progressEndValue = 91,
+      speed = 130;
+
+let progress = setInterval(() =>{
+        progressStartValue++;
+
+        progressValue.textContent = `${progressStartValue}%`
+        circularProgress.style.background = `conic-gradient(#0ad959 ${progressStartValue * 3.6}deg, #ededed 0deg)`
+        if(progressStartValue == progressEndValue){
+            clearInterval(progress);
+        }
+}, speed);
